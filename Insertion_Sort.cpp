14930f51ba942c24d304ece991be087346191a55
RifalArdi25 <rifalardi25@gmail.com>
2025-03-22 17:13:49 +0700

    Menambahkan Prosedur Input

--- Insertion_Sort.cpp
+++ Insertion_Sort.cpp
@@ -3,3 +3,30 @@ using namespace std;
 
 int arr[20]; //Membuat array dengan panjang data 20
 int n; //Membuat variabel input n
+
+void input()
+{
+    while (true)
+    {
+        cout << "Masukkan Jumlah Data pada Array : "; //Membuat inputan jumlah data
+        cin >> n; //Memanggil variabel inputan n
+        if (n <= 20)
+        { //Membuat Kondisi n tidak lebih dari 20 
+            break;
+        }
+        else
+        {
+            cout << "\nArray yang anda masukkan maksimal 20 Elemen.\n"; //Menampilkan Pesan jika data lebih dari 20
+        }
+    }
+    cout << endl; //Membuat jarak per baris program
+    cout << "======================" << endl; //Membuat tampilan susunan data element array
+    cout << "Masukkan Element Array" << endl;
+    cout << "======================" << endl;
+
+    for (int i = 0; i < n; i++) //Menggunakan perulangan for untuk menyimpan data pada array
+    {
+        cout << "Data ke-" << (i + 1) << ": "; //Memasukkan atau menginputkan nilai data n
+        cin >> arr[i]; //Menyimpan nilai data n kedalam array arr
+    }
+}
