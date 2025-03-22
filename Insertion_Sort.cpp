cb5a91c0f575511285c067a833ec06765188346b
RifalArdi25 <rifalardi25@gmail.com>
2025-03-22 17:38:39 +0700

    Merapikan Komentar

--- Insertion_Sort.cpp
+++ Insertion_Sort.cpp
@@ -1,17 +1,17 @@
 #include <iostream>
 using namespace std;
 
-int arr[20]; //Membuat array dengan panjang data 20
-int n; //Membuat variabel input n
+int arr[20]; // Membuat array dengan panjang data 20
+int n; //  Membuat variabel input n
 
 void input()
 {
     while (true)
     {
-        cout << "Masukkan Jumlah Data pada Array : "; //Membuat inputan jumlah data
-        cin >> n; //Memanggil variabel inputan n
+        cout << "Masukkan Jumlah Data pada Array : "; // Membuat inputan jumlah data
+        cin >> n; // Memanggil variabel inputan n
         if (n <= 20)
-        { //Membuat Kondisi n tidak lebih dari 20 
+        { // Membuat Kondisi n tidak lebih dari 20 
             break;
         }
         else
@@ -19,55 +19,55 @@ void input()
             cout << "\nArray yang anda masukkan maksimal 20 Elemen.\n"; //Menampilkan Pesan jika data lebih dari 20
         }
     }
-    cout << endl; //Membuat jarak per baris program
-    cout << "======================" << endl; //Membuat tampilan susunan data element array
+    cout << endl; // Membuat jarak per baris program
+    cout << "======================" << endl; // Membuat tampilan susunan data element array
     cout << "Masukkan Element Array" << endl;
     cout << "======================" << endl;
 
-    for (int i = 0; i < n; i++) //Menggunakan perulangan for untuk menyimpan data pada array
+    for (int i = 0; i < n; i++) // Menggunakan perulangan for untuk menyimpan data pada array
     {
-        cout << "Data ke-" << (i + 1) << ": "; //Memasukkan atau menginputkan nilai data n
-        cin >> arr[i]; //Menyimpan nilai data n kedalam array arr
+        cout << "Data ke-" << (i + 1) << ": "; // Memasukkan atau menginputkan nilai data n
+        cin >> arr[i]; // Menyimpan nilai data n kedalam array arr
     }
 }
 
 void insertionSort()
-{//Procedure Insertionsort
+{ // Procedure Insertionsort
 
-    int temp; //Membuat variabel data temporer atau penyimpanan data sementara
-    int j, i; //Membuat variabel j sebagai penanda
+    int temp; // Membuat variabel data temporer atau penyimpanan data sementara
+    int j, i; // Membuat variabel j sebagai penanda
 
     for (i = 1; i <= n - 1; i++)
-    {//step 1
+    { // step 1
 
-        temp = arr[i]; //step 2
+        temp = arr[i]; // step 2
 
-        j = i - 1; //step 3
+        j = i - 1; // step 3
 
-        while (j >= 0 && arr[j] > temp) //step 4
+        while (j >= 0 && arr[j] > temp) // step 4
         {
-            arr[j + 1] = arr[j]; //step 4a
-            j--; //step 4b
+            arr[j + 1] = arr[j]; // step 4a
+            j--; // step 4b
         }
 
-        arr[j + 1] = temp; //step 5 
+        arr[j + 1] = temp; // step 5 
     }
 }
 
 void display()
 {
-    cout << endl; //Output baris kosong
+    cout << endl; // Output baris kosong
     cout << "==============================" << endl;
-    cout << "Total Pass = " << n - 1 << endl; //Count element movement
-    cout << "==============================" << endl; //Output ke layar
-    cout << "Element Array yang telah tersusun" << endl; //Output ke layar
-    cout << "==============================" << endl; //Output ke layar
+    cout << "Total Pass = " << n - 1 << endl; // Count element movement
+    cout << "==============================" << endl; // Output ke layar
+    cout << "Element Array yang telah tersusun" << endl; // Output ke layar
+    cout << "==============================" << endl; // Output ke layar
 
     for (int j = 0; j < n; j++)
-    { //Looping dengan j dimulai dari 0 hingga n-1
-        cout << arr[j] << endl; //Output ke layar 
+    { // Looping dengan j dimulai dari 0 hingga n-1
+        cout << arr[j] << endl; // Output ke layar 
     }
-    cout << endl; //Output baris kosong
+    cout << endl; // Output baris kosong
 }
 
 int main()
