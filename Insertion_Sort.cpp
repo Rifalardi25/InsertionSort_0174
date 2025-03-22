e5cc5abe9203fb2223fdf5a8cd45f5ecf5231ee0
RifalArdi25 <rifalardi25@gmail.com>
2025-03-22 17:22:29 +0700

    Menambahkan Prosedur Insertion Sort

--- Insertion_Sort.cpp
+++ Insertion_Sort.cpp
@@ -30,3 +30,27 @@ void input()
         cin >> arr[i]; //Menyimpan nilai data n kedalam array arr
     }
 }
+
+void insertionSort()
+{//Procedure Insertionsort
+
+    int temp; //Membuat variabel data temporer atau penyimpanan data sementara
+    int j, i; //Membuat variabel j sebagai penanda
+
+    for (i = 1; i <= n - 1; i++)
+    {//step 1
+
+        temp = arr[i]; //step 2
+
+        j = i - 1; //step 3
+
+        while (j >= 0 && arr[j] > temp) //step 4
+        {
+            arr[j + 1] = arr[j]; //step 4a
+            j--; //step 4b
+        }
+
+        arr[j + 1] = temp; //step 5 
+    }
+}
+
